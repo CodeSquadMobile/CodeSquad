cd9b5cd22e9606b88385f4a782db8239efb83321
Marcelo Luna <ldl_marcelo@hotmail.com>
2024-09-12 18:57:01 -0300

    agrego botones y pruebo Intent

--- app/src/main/java/com/example/mercadolibromobile/MainActivity.java
+++ app/src/main/java/com/example/mercadolibromobile/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.mercadolibromobile;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+       Button myButton = findViewById(R.id.dashb);
+        myButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, dashboardUsuario.class);
+            startActivity(intent);
         });
     }
 }
