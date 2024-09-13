d1a4f7253eaa71122e02cf102f94b1653d202007
Leopladom <114880850+Leopladom@users.noreply.github.com>
2024-09-13 18:37:29 -0300

    hola

--- app/src/main/java/com/example/mercadolibromobile/fragment_Finalizar.java
+++ app/src/main/java/com/example/mercadolibromobile/fragment_Finalizar.java
@@ -21,8 +21,8 @@ public class fragment_Finalizar extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String param1;
+    private String param2;
 
     public fragment_Finalizar() {
         // Required empty public constructor
@@ -50,8 +50,8 @@ public class fragment_Finalizar extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            param1 = getArguments().getString(ARG_PARAM1);
+            param2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
