de8a1cf4aeeb4331330b6c42e900567526134cad
nahir1009 <nahir1009@gmail.com>
2024-09-13 23:23:06 -0300

    mod

--- app/src/main/java/com/example/mercadolibromobile/finalizarcomprafragment.java
+++ app/src/main/java/com/example/mercadolibromobile/finalizarcomprafragment.java
@@ -11,11 +11,11 @@ import android.view.ViewGroup;
 
 import org.jetbrains.annotations.Nullable;
 
-public class FinalizarCompraFragment extends Fragment {
+public class finalizarcomprafragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_finalizarCompra, container, false);
+        return inflater.inflate(R.layout.fragment_finalizarcompra, container, false);
     }
 }
