996550dd5f4fd3ff6e234487f10a4d3b1f93c772
Ivette Jael Nobiltá <100625720+IvetteJN@users.noreply.github.com>
2024-09-14 21:39:00 -0300

    connected login activity

--- app/src/main/java/com/example/mercadolibromobile/LoginActivity.java
+++ app/src/main/java/com/example/mercadolibromobile/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.mercadolibromobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -9,5 +12,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Botón de login (que por ahora no requiere autenticación)
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navega a MainActivity sin autenticación
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();  // Termina LoginActivity para no volver atrás
+            }
+        });
     }
 }
