a60281d17eee59dc694491b4443023906c58562e
nahir1009 <nahir1009@gmail.com>
2024-10-14 18:19:31 -0300

    api conectada

---
+++ app/src/main/java/com/example/mercadolibromobile/models/AuthModels.java
@@ -0,0 +1,67 @@
+package com.example.mercadolibromobile.models;
+
+public class AuthModels {
+
+    public static class LoginRequest {
+        private String email;
+        private String password;
+
+        public LoginRequest(String email, String password) {
+            this.email = email;
+            this.password = password;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+
+    public static class LoginResponse {
+        private String access;
+        private String refresh;
+
+        public String getAccess() {
+            return access;
+        }
+
+        public String getRefresh() {
+            return refresh;
+        }
+    }
+
+    public static class SignupRequest {
+        private String email;
+        private String password;
+        private String username;
+
+        public SignupRequest(String email, String password, String username) {
+            this.email = email;
+            this.password = password;
+            this.username = username;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+    }
+
+    public static class SignupResponse {
+        private String message;
+
+        public String getMessage() {
+            return message;
+        }
+    }
+}
