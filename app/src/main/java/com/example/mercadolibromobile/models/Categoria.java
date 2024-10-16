ad3c644f448e1cd2d1c998302336bbabb9133388
Ivette Jael Nobiltá <100625720+IvetteJN@users.noreply.github.com>
2024-10-16 12:15:46 -0300

    Create Categoria.java

---
+++ app/src/main/java/com/example/mercadolibromobile/models/Categoria.java
@@ -0,0 +1,13 @@
+package com.example.mercadolibromobile.models;
+
+public class Categoria {
+    private String nombre_categoria;
+
+    public String getNombreCategoria() {
+        return nombre_categoria;
+    }
+
+    public void setNombreCategoria(String nombre_categoria) {
+        this.nombre_categoria = nombre_categoria;
+    }
+}
