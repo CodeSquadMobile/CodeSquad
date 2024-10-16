815960a60e569292e940c89fe5a75aae4c307a73
Ivette Jael Nobiltá <100625720+IvetteJN@users.noreply.github.com>
2024-10-16 12:11:34 -0300

    Update BookApi.java para la búsqueda y el filtro por categoría

--- app/src/main/java/com/example/mercadolibromobile/api/BookApi.java
+++ app/src/main/java/com/example/mercadolibromobile/api/BookApi.java
@@ -1,12 +1,10 @@
-package com.example.mercadolibromobile.api;
 import com.example.mercadolibromobile.models.Book;
-
+import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import java.util.List;
+import retrofit2.http.Query;
 
 public interface BookApi {
     @GET("libros/")
-    Call<List<Book>> getBooks();
+    Call<List<Book>> getBooks(@Query("titulo") String titulo, @Query("id_categoria__nombre_categoria") String categoria);
 }
-
