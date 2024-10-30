441310eb0d860c789cf59a6525ea6ed7037920bc
Leopladom <114880850+Leopladom@users.noreply.github.com>
2024-10-30 00:51:41 -0300

    ...

--- app/src/main/java/com/example/mercadolibromobile/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/mercadolibromobile/fragments/ProfileFragment.java
@@ -1,25 +1,25 @@
 package com.example.mercadolibromobile.fragments;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.mercadolibromobile.R;
-import com.example.mercadolibromobile.MisResenasActivity; // Asegúrate de importar la actividad
+import com.example.mercadolibromobile.api.ApiService;
 
 public class ProfileFragment extends Fragment {
-
     private TextView emailTextView;
+    private Button deleteAccountButton;
+    private ApiService userService;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -32,18 +32,15 @@ public class ProfileFragment extends Fragment {
         // Obtener el correo electrónico desde SharedPreferences
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE);
         String userEmail = sharedPreferences.getString("user_email", "No email found");
-
-        // Mostrar el correo electrónico en el TextView
         emailTextView.setText(userEmail);
 
-        // Configurar el botón "Mis Reseñas"
-        Button reviewsButton = rootView.findViewById(R.id.button2);
-        reviewsButton.setOnClickListener(new View.OnClickListener() {
+        // Configurar el botón "Dar de Baja Usuario"
+        deleteAccountButton = rootView.findViewById(R.id.button9);
+        deleteAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Abrir MisResenasActivity
-                Intent intent = new Intent(getActivity(), MisResenasActivity.class);
-                startActivity(intent); // Iniciar la actividad
+//                String accessToken = getAccessToken();
+//                deleteUser(accessToken);
             }
         });
 
