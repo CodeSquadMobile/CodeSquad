1b972736b7509cbeab280feaa93f3c4df9bfba6f
Ivette Jael Nobiltá <100625720+IvetteJN@users.noreply.github.com>
2024-11-02 10:19:53 -0300

    Update and rename ApiClient2.java to ApiClient.java

--- app/src/main/java/com/example/mercadolibromobile/api/ApiClient.java
+++ app/src/main/java/com/example/mercadolibromobile/api/ApiClient.java
@@ -4,7 +4,7 @@ package com.example.mercadolibromobile.api;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class ApiClient2 {
+public class ApiClient {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
